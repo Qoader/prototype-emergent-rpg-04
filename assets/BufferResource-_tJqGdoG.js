09e8a2225bd388902870c11e53013f66f15b23a3
Qoader <Qoader@users.noreply.github.com>
2026-09-02 15:54:42 +0000

    deploy: d3c11834c226ac3807e80d6f71b234ab032cd8b5

--- assets/BufferResource-_tJqGdoG.js
+++ assets/BufferResource-_tJqGdoG.js
@@ -1,4 +1,4 @@
-import{w as _,d as g,e as c,u as U,B as y,f as l,E as I,h as m}from"./index-BVN_fwAd.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,d as g,e as c,u as U,B as y,f as l,E as I,h as m}from"./index-CSw8EQ6-.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
