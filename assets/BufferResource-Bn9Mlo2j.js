02139f1c5c9a9b266d8e252c5a331b74098c2950
Qoader <Qoader@users.noreply.github.com>
2026-09-05 21:18:12 +0000

    deploy: 6f321c3ef6fbf145a3fa0a80a932b92ba402fb5e

--- assets/BufferResource-Bn9Mlo2j.js
+++ assets/BufferResource-Bn9Mlo2j.js
@@ -1,4 +1,4 @@
-import{w as _,d as g,e as c,u as U,B as y,f as l,E as I,h as m}from"./index-C2WyrWhY.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,d as g,e as c,u as U,B as y,f as l,E as I,h as m}from"./index-y_LU98Ng.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
