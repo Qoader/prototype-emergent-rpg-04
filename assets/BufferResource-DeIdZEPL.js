d20f59e040c75f97605b0e71da502bb7a09d7623
Qoader <Qoader@users.noreply.github.com>
2026-09-10 08:40:52 +0000

    deploy: 5eb9b519b2089eae61a21bbee35410003f62d18b

--- assets/BufferResource-DeIdZEPL.js
+++ assets/BufferResource-DeIdZEPL.js
@@ -1,4 +1,4 @@
-import{w as _,d as g,e as c,u as U,B as y,f as l,E as I,h as m}from"./index-De_iJ1MV.js";const h={name:"local-uniform-bit",vertex:{header:`
+import{w as _,d as g,e as c,u as U,B as y,f as l,E as I,h as m}from"./index-l0I6umOs.js";const h={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
